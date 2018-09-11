1401bf3b40a99b0315ca1bf01aecdd74a7907974
Rei Colina <reinaldo13@gmail.com>
2018-09-10 22:23:34 -0700

    Update content.js

--- ie-demo/src/js/content.js
+++ ie-demo/src/js/content.js
@@ -1,2 +1,13 @@
+var error = function() {
+    console.log("error!");
+};
+var callback = function(content) {
+    console.log('Backgound script responded with: ' + content);
+};
+var message = 'Hello there!';
+
 console.log('this is the content script!');
 alert('this is the content script!');
+
+console.log("Sending message '" + message + "' to the background script...");
+forge.message.broadcastBackground("background-message", message, callback, error);
