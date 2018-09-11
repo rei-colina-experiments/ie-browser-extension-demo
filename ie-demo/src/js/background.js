e6f006a33fc821d1b08649560a206357e6f1dc20
Rei Colina <reinaldo13@gmail.com>
2018-09-10 22:24:27 -0700

    Update background.js

--- ie-demo/src/js/background.js
+++ ie-demo/src/js/background.js
@@ -1,2 +1,11 @@
+var error = function() {
+    console.log("error!");
+};
+var callback = function(content, reply) {
+    reply("Backgound job got the message '" + content + "', and replied back!");
+};
+
+forge.message.listen("background-message", callback, error);
 console.log('i am in the background..yay!');
+
 alert('This is the background script running..yay!');
